0c6cbe985e18875660e6aebedc35fe26694acd0d
Niek Saarberg <nsaarberg@xebia.com>
2022-09-01 15:27:19 +0200

    eslint tweaks

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,16 +2,26 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   // Specifies the ESLint parser
-  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "prettier", "plugin:prettier/recommended", "plugin:react/recommended", "plugin:storybook/recommended"],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'plugin:prettier/recommended',
+    'plugin:react/recommended',
+    'plugin:storybook/recommended',
+    'plugin:unicorn/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:react/jsx-runtime',
+  ],
   parserOptions: {
     ecmaVersion: 2018,
     // Allows for the parsing of modern ECMAScript features
     sourceType: 'module',
     // Allows for the use of imports
     ecmaFeatures: {
-      jsx: true // Allows for the parsing of JSX
-
-    }
+      jsx: true, // Allows for the parsing of JSX
+    },
   },
   plugins: ['@typescript-eslint', 'react-hooks'],
   rules: {
@@ -22,11 +32,14 @@ module.exports = {
     '@typescript-eslint/no-var-requires': 'off',
     'react/prop-types': 'off',
     'react/jsx-uses-react': 'off',
-    'react/react-in-jsx-scope': 'off'
+    'react/react-in-jsx-scope': 'off',
+    'unicorn/filename-case': 'off',
+    'unicorn/no-null': 'off',
+    'unicorn/prevent-abbreviations': 'off',
   },
   settings: {
     react: {
-      version: 'detect'
-    }
-  }
+      version: 'detect',
+    },
+  },
 };
