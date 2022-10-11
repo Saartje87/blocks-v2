6d309d872a403d74a0c8468b4f39134d828718bd
Niek Saarberg <nsaarberg@xebia.com>
2022-10-11 10:48:38 +0200

    Using heroicons under the hood

--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -4,55 +4,10 @@ import { classnames } from '../../utils';
 import { useBlocksContext } from '../BlocksProvider/useBlocksContext';
 import { Box } from '../Box';
 import * as styles from './Icon.css';
+import { IconNames } from './iconNames';
 
 export type IconProps = {
-  name:
-    | 'arrowdown'
-    | 'calendar'
-    | 'delete'
-    | 'heart'
-    | 'more'
-    | 'plus'
-    | 'show'
-    | 'user'
-    | 'audio'
-    | 'camera'
-    | 'download'
-    | 'hide'
-    | 'night'
-    | 'poll'
-    | 'smile'
-    | 'walk'
-    | 'back'
-    | 'car'
-    | 'dropdown'
-    | 'home'
-    | 'nutrition'
-    | 'search'
-    | 'sparkle'
-    | 'water'
-    | 'badge'
-    | 'comment'
-    | 'feed'
-    | 'lock'
-    | 'pause'
-    | 'send'
-    | 'star'
-    | 'weight'
-    | 'bookmark'
-    | 'copy'
-    | 'fire'
-    | 'message'
-    | 'place'
-    | 'setting'
-    | 'swim'
-    | 'bus'
-    | 'day'
-    | 'health'
-    | 'minus'
-    | 'play'
-    | 'share'
-    | 'time';
+  name: IconNames;
   size?: keyof typeof styles.sizes;
   className?: string;
   color?: Atoms['color'];
--- src/components/Icon/index.ts
+++ src/components/Icon/index.ts
@@ -1,1 +1,2 @@
 export * from './Icon';
+export { IconNames } from './iconNames';
